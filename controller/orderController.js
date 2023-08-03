f16c1a5b85f2e4dde077b78928f1c262e7f0dd98
Bryan Matthew <bryanmatthew206@gmail.com>
2023-08-03 22:04:55 +0700

    feat: add endpoint for order
    1. GET /orders to get the user's order history
    2 GET /orders/:id to get the order detail
    3. PATCH /orders to update the order status

---
+++ controller/orderController.js
@@ -0,0 +1,49 @@
+const { Order, OrderDetail, Product } = require('../models');
+
+class Controller {
+    static async updateStatusOrder(req, res, next) {
+        try {
+            const id = req.params.id;
+            const order = await Order.findByPk(id);
+            if (!order) throw { name: "NotFound" };
+            await order.update({ paymentStatus: "paid" });
+            res.status(200).json({ message: "payment status has been updated" });
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async getOrderHistory(req, res, next) {
+        try {
+            const orders = await Order.findAll({
+                where: {
+                    UserId: req.user.id,
+                    paymentStatus: 'paid',
+                },
+                order: [['updatedAt', 'DESC']]
+            });
+            res.status(200).json(orders);
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    static async getOrderDetail(req, res, next) {
+        try {
+            const orderId = req.params.id;
+            const orderDetail = await OrderDetail.findAll({
+                where: { OrderId: orderId },
+                include: {
+                    model: Product
+                }
+            });
+
+            res.status(200).json(orderDetail);
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
+    }
+}
+
+module.exports = Controller;
