20d52b4af03a4f8eddf70cce09a03b7c13eb1d20
Bryan Matthew <bryanmatthew206@gmail.com>
2023-08-03 22:14:19 +0700

    feat: add midtrans payment route to generate payment token

---
+++ controller/paymentController.js
@@ -0,0 +1,49 @@
+const midtransClient = require('midtrans-client');
+const { Cart, Order, OrderDetail } = require('../models');
+
+
+class Controller {
+    static async processPayment(req, res, next) {
+        try {
+            const { amount } = req.body;
+            if (!amount) throw { name: "InvalidPaymentAmount" }
+
+            const cart = await Cart.findAll({ where: { UserId: req.user.id } });
+            const order = await Order.create({ UserId: req.user.id });
+            const orderDetail = cart.map((el) => {
+                return {
+                    OrderId: order.id,
+                    ProductId: el.ProductId,
+                    quantity: el.quantity,
+
+                }
+            })
+            await OrderDetail.bulkCreate(orderDetail);
+
+            let snap = new midtransClient.Snap({
+                isProduction: false,
+                serverKey: `${process.env.MIDTRANS_SERVER_KEY}`
+            });
+
+            let parameter = {
+                "transaction_details": {
+                    "order_id": `order-${order.id}`,
+                    "gross_amount": `${amount}`
+                },
+                "credit_card": {
+                    "secure": true
+                },
+                "customer_details": {
+                    "email": `${req.user.email}`,
+                }
+            };
+
+            const midtransToken = await snap.createTransaction(parameter);
+            res.status(200).json({ midtransToken, orderId: order.id });
+        } catch (err) {
+            next(err);
+        }
+    }
+}
+
+module.exports = Controller;
